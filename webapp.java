6fa050f171c776c26a2936c44825e0ecb218f6da
yugant6 <34997974+yugant6@users.noreply.github.com>
2019-02-07 11:48:32 +0530

    Create webapp.java

---
+++ webapp.java
@@ -0,0 +1,30 @@
+package edu.kiet.www.mykietapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+        super.onBackPressed();
+    }}
+}
